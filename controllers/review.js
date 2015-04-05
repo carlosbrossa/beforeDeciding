157d33aa3dd8f26cb928b272806e86aae5dbb699
Romeu Ivolela <romeu@Romeus-MacBook-Pro.local>
2015-04-05 15:37:34 -0300

    merge do repositorio git com heroku

--- controllers/review.js
+++ controllers/review.js
@@ -3,19 +3,24 @@
 module.exports = function(app) {
 
   var http = require('http');
+  
+  //refatorar a passar para um servico separado
+  var aws = require("aws-lib");
+  var accessKeyId = "AKIAI6Z6PT3MZYS4ZKLA";
+  var secretAccessKey = "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";
+  var associateTag = "beforedecidin-20";
+  var prodAdv = aws.createProdAdvClient(accessKeyId, secretAccessKey, associateTag);
 
-   //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-  var options = {
-    host: 'api.walmartlabs.com',
-    path: '/v1/search?format=json&apiKey=j3sp77bmf7ywymrdx78dq6bj'
-  };
 
-  var optionsReview = {
-    host: 'api.walmartlabs.com',
-    path: '/v1/reviews/itemId?format=json&apiKey=j3sp77bmf7ywymrdx78dq6bj'
-  };
+  var buscarProduto = function(product, res){
 
-  var buscarProduto = function(options, res){
+    //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+    var options = {
+      host: 'api.walmartlabs.com',
+      path: '/v1/search?format=json&apiKey=j3sp77bmf7ywymrdx78dq6bj'
+    };
+    options.path = options.path + '&query=' + product;
+
     http.get(options, function(response) {
       var str = '';
 
@@ -44,6 +49,10 @@ module.exports = function(app) {
   }; 
 
   var buscarReview = function(res, objProduct){
+    var optionsReview = {
+      host: 'api.walmartlabs.com',
+      path: '/v1/reviews/itemId?format=json&apiKey=j3sp77bmf7ywymrdx78dq6bj'
+    };
     optionsReview.path = optionsReview.path.replace('itemId', objProduct.itemId);
     http.get(optionsReview, function(response) {
 
@@ -58,7 +67,8 @@ module.exports = function(app) {
         str = JSON.parse(str);
         objProduct.reviews = str.reviews;
 
-        res.send(objProduct);
+        //res.send(objProduct);
+	res.jsonp(objProduct);
       });
 
     }).end();  
@@ -66,9 +76,17 @@ module.exports = function(app) {
   };
 
   var teste = function(req, res){
-    options.path = options.path + '&query=' + req.product;        
-    buscarProduto(options, res);
+    //options.path = options.path + '&query=' + req.product;
+    //console.log(options.path);        
+    buscarProduto(req.product, res);
   };
+
+  var amazon = function(req, res){
+    prodAdv.call("ItemSearch", {SearchIndex: "Books", Keywords: "Javascript"}, function(err, result) {
+      res.status(200);
+      res.send(result);      
+    });
+  }
 
   app.param('product', function(req, res, next, product) {  
     req.product = product;
@@ -79,12 +97,20 @@ module.exports = function(app) {
   var ReviewController = {
     search : function(req, res) {
       console.log('1');
-      teste(req, res);
-	/*Joke.findOne(function(err, jokeFind){
-		res.status(200);
-		res.send(jokeFind);
-	});	*/
+      teste(req, res);    
+    },
+    amazon : function(req, res) {
+      console.log('1');
+      //teste(req, res);
+      amazon(req, res);
+
+  /*Joke.findOne(function(err, jokeFind){
+    res.status(200);
+    res.send(jokeFind);
+  }); */
     }
+
+
   }
   return ReviewController;
 
--- routes/review.js
+++ routes/review.js
@@ -3,6 +3,6 @@ module.exports = function(app) {
   var review = app.controllers.review;
 
   app.get('/review/:product', review.search);
-
+  app.get('/amazon/:product', review.amazon);
 };
 
