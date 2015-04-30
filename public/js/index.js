8b124ba6ed75bc21de941614eb488b58cc6a48a6
Romeu Ivolela <romeu@Romeus-MacBook-Pro.local>
2015-04-30 19:13:08 -0300

    correcao de urls de serviços

--- public/js/index.js
+++ public/js/index.js
@@ -6,7 +6,7 @@ $( "#target" ).submit(function( event ) {
 
    var search = $("#busca").val();
 
-   window.location.replace("http://localhost:5600/searchreviews.html?product=" + search);
+   window.location.replace("/searchreviews.html?product=" + search);
 
 })
 
